79c89ca80a9fdccf2b8da421df36eefc5d707944
VeejaLiu <veejaliu@gmail.com>
2023-08-11 15:39:08 +0800

    添加对数变换

--- code/ch03/02/image_logarithm_transformation.py
+++ code/ch03/02/image_logarithm_transformation.py
@@ -7,6 +7,18 @@ image = cv2.imread('images/DFT_no_log.tif', cv2.IMREAD_GRAYSCALE)
 # s = c * log(1 + r)
 c = 1
 image_log = c * np.log1p(image)
+# image_log = np.array(image_log, dtype=np.uint8) # 转换数据类型
+
+max_pixel = np.max(image_log)
+min_pixel = np.min(image_log)
+
+print('max_pixel: ', max_pixel)
+print('min_pixel: ', min_pixel)
+
+# 将图像线性缩放到 0-255
+image_log = (image_log - min_pixel) / (max_pixel - min_pixel) * 255
+
+# 转换数据类型
 image_log = np.array(image_log, dtype=np.uint8) # 转换数据类型
 
 # 将原始图像和对数变换后的图像拼接在一起
